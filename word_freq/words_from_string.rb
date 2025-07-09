2a4b3d186bc318505232aad70c40e5d6da474792
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-07-09 10:42:39 -0600

    Fix #better_word_count

--- word_freq/words_from_string.rb
+++ word_freq/words_from_string.rb
@@ -2,4 +2,4 @@ def words_from_string(string)
   string.downcase.scan(/[\w']+/)
 end
 
-p words_from_string("I like Ruby, it is (usually) optimized for programmer happiness")
+#p words_from_string("I like Ruby, it is (usually) optimized for programmer happiness")
