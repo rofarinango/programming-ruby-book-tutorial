e0a83607554d8858dc84db57fe00465c3dba3354
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-07-10 11:46:34 -0600

    Add method names

--- chapter_5/defining_methods.rb
+++ chapter_5/defining_methods.rb
@@ -11,3 +11,17 @@ class Batman
 end
 
 Batman.new.who_is_robin
+
+# In a "regular" method definition, method body starts on the line after the method declaration and continues until a matching end.
+
+def a_method_name(arg)
+  puts arg
+end
+
+# You can also create one-line methods called "endless method" (because you dont use the end statement)
+
+def a_method_name(arg) = puts args
+
+# Sometimes you'll define a method with no body. (Often the method will be fully defined by a subclass)
+
+def a_method_name; end
